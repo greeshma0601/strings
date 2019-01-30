3da333228e1328ce609a0e12d21a45b2b39a68b3
greeshma0601 <40259116+greeshma0601@users.noreply.github.com>
2019-01-30 23:28:48 +0530

    Create Longest Prefix Suffix.cpp

---
+++ Longest Prefix Suffix.cpp
@@ -0,0 +1,76 @@
+/*
+Longest Prefix Suffix
+Given a string of character, find the length of longest proper prefix which is also a proper suffix.
+Example:
+S = abab
+lps is 2 because, ab.. is prefix and ..ab is also a suffix.
+
+Input:
+First line is T number of test cases. 1<=T<=100.
+Each test case has one line denoting the string of length less than 100000.
+
+Expected time compexity is O(N).
+
+Output:
+Print length of longest proper prefix which is also a proper suffix.
+
+Example:
+Input:
+2
+abab
+aaaa
+
+Output:
+2
+3
+*/
+
+
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main()
+ {
+ int t;
+ cin>>t;
+ while(t--)
+ {
+ string pat;
+ cin>>pat;
+ int M=pat.length();
+ int lps[M];
+ int len = 0; 
+  
+    lps[0] = 0; // lps[0] is always 0 
+  
+    // the loop calculates lps[i] for i = 1 to M-1 
+    int i = 1; 
+    while (i < M) { 
+        if (pat[i] == pat[len]) { 
+            len++; 
+            lps[i] = len; 
+            i++; 
+        } 
+        else // (pat[i] != pat[len]) 
+        { 
+            // This is tricky. Consider the example. 
+            // AAACAAAA and i = 7. The idea is similar 
+            // to search step. 
+            if (len != 0) { 
+                len = lps[len - 1]; 
+  
+                // Also, note that we do not increment 
+                // i here 
+            } 
+            else // if (len == 0) 
+            { 
+                lps[i] = 0; 
+                i++; 
+            } 
+        } 
+    } 
+    cout<<lps[M-1]<<endl;
+ }
+	//code
+	return 0;
+}
